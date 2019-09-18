cab7f3d5336ebe92f65b3d743c4b8dc938c6368f
Maxime <justine.gander@gmail.com>
2019-09-18 09:26:13 +0200

    lab done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import fullListOfContacts from './contacts.json'
 import './App.css';
 
 function App() {
+  const [contacts, setContacts] = useState(fullListOfContacts.slice(0,5));
+  function addRandom(){
+    setContacts([...contacts, fullListOfContacts[Math.floor(Math.random() * fullListOfContacts.length)]])
+  }
+  
+function sortName(){
+  setContacts( [...contacts].sort((a,b)=> {
+    if (a.name>b.name) return 1;
+    else return -1
+  }))
+}
+
+function sortPopularity(){
+  setContacts( [...contacts].sort((a,b)=> {
+    if (a.popularity>b.popularity) return -1;
+    else return 1
+  }))
+}
+
+function deleteButton(index){
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <button onClick ={()=> addRandom()}>Add random contact</button>
+      <button onClick ={()=> sortName()}>Sort by name</button>
+      <button onClick ={()=> sortPopularity()}>Sort by popularity</button>
+      <hr/>
+      <table>
+        <thead>
+            <tr>
+                <th><h1>Picture</h1></th>
+                <th><h1>Name</h1></th>
+                <th><h1>Popularity</h1></th>
+                <th><h1>Action</h1></th>
+            </tr>
+        </thead>
+        <tbody>
+                {contacts.map((element, i) => <tr key={i}> 
+                <td><img src={element.pictureUrl} alt="img"/></td>
+                <td>{element.name}</td>
+                <td>{element.popularity.toFixed(2)}</td> <button onClick ={()=> deleteButton()}>Delete</button></tr>)}
+                
+        </tbody>
+      </table>
     </div>
   );
 }
